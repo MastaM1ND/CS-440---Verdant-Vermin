f9e680390a8111fbfcf835e148352b3342bdf3ba
“komi-ai” <ka00049@Mix.wvu.edu>
2025-04-15 14:30:36 -0400

    change of db

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'KarachixMorganT1!',  
+  password: 'youShallPass1!',  
   database: 'study_group_finder'
 });
 
